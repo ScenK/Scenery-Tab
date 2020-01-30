069d815bacdd434080a138a7e82b19ee98327db7
Scen <scen.dete@gmail.com>
2020-01-30 13:45:48 -0600

    change weather provider

--- src/background.js
+++ src/background.js
@@ -1,7 +1,7 @@
 class SceneryTab {
 
   constructor() {
-    this.debug = false
+    this.debug = true
     this.wp = new Wallpaper()
     this.wt = new Weather()
     this.ti = new Time()
@@ -18,87 +18,72 @@ class SceneryTab {
   }
 
   async setWeather(cached = false) {
-    const weatherData = cached ? await this.wt.getWeatherDataCache('CurrentWeather') : await this.wt.getCurrentWeather()
+    const weatherData = cached ? JSON.parse(await this.wt.getWeatherDataCache('CurrentWeather')) : await this.wt.getCurrentWeather()
     if (!weatherData) {
       return
     }
 
-    if (!cached) {
-      this.set5DaysWeather()
+    if (!cached && weatherData) {
+      this.set5DaysWeather(weatherData)
     }
     if (this.debug) {
       console.debug('weatherData', weatherData)
     }
 
+    const curretTemp = Math.floor(weatherData.current_observation.condition.temperature)
+    const icon = this.wt.weatherIcon[weatherData.current_observation.condition.code]
+    const currentCondition = weatherData.current_observation.condition.text
+
     try {
-      // set today
-      document.querySelector('.top a').title = weatherData.weather[0].main
-      document.querySelector('.top .main-icon img').src = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
-      document.querySelector('.top .detail .current').innerHTML = `${Math.floor(weatherData.main.temp)}&#8457`
+      // set today's info in the detail container
+      document.querySelector('.top .date').textContent = new Date().toLocaleDateString('en-US', {weekday: 'long'})
+      document.querySelector('.top a').title = currentCondition
+      document.querySelector('.top .main-icon .wi').classList.add(icon)
+      document.querySelector('.top .detail .current').innerHTML = `${curretTemp}&#8457`
 
       // set main
-      document.getElementById('temp').innerHTML = Math.floor(weatherData.main.temp)
-      document.querySelector('#weather-icon img').src = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
-      document.querySelector('#weather-icon a').title = weatherData.weather[0].main
-      document.getElementById('location').textContent = weatherData.name
+      document.getElementById('temp').innerHTML = curretTemp
+      document.querySelector('#weather-icon .wi').classList.add(icon)
+      document.querySelector('#weather-icon a').title = currentCondition
+      document.getElementById('location').textContent = weatherData.location.city
 
     } catch (err) {
 
     }
   }
 
-  async set5DaysWeather() {
-    const weatherData = await this.wt.get5DaysWeather()
-    const fiveDaysData = []
-
-    for (let i = 0; i < weatherData.list.length; i++) {
-      const day = weatherData.list[i].dt_txt.split(' ')[0]
-      const icon = weatherData.list[i].weather[0].icon
-      const high = weatherData.list[i].main.temp_max
-      const low = weatherData.list[i].main.temp_min
-
-      const included = fiveDaysData.filter((i) => i.day === day)
-      if (included.length > 0) {
-        included[0].high = included[0].high > high ? included[0].high : high
-        included[0].low = included[0].low < low ? included[0].low : low
-      } else {
-        fiveDaysData.push({
-          day,
-          icon,
-          high,
-          low
-        })
-      }
-    }
-
-    if (this.debug) {
-      console.debug('5DaysWeatherData', fiveDaysData)
-    }
-
+  async set5DaysWeather(weatherData) {
     const container = document.getElementById('full-weather')
+
+    // clean old elements
+    document.querySelectorAll(".day-clone").forEach(e => e.parentNode.removeChild(e));
+
+    const fiveDaysData = weatherData.forecasts.slice(0, 6)
+
     for (let i = 0; i < fiveDaysData.length; i++) {
-      const date = fiveDaysData[i].day.split('-')
-      const formatedDate = `${date[1]}/${date[2]}/${date[0]}`
-      const icon = fiveDaysData[i].icon
+      const date = fiveDaysData[i].day
+      const icon = this.wt.weatherIcon[fiveDaysData[i].code]
       const high = fiveDaysData[i].high
       const low = fiveDaysData[i].low
+      const text = fiveDaysData[i].text
 
       if (i === 0) {
-        // set today
-        document.querySelector('.top .date').textContent = new Date(formatedDate).toLocaleString(chrome.i18n.getUILanguage(), {weekday:'long'})
-        document.querySelector('.top .main-icon img').src = `https://openweathermap.org/img/wn/${icon}@2x.png`
+        // set today's additional info in the detail container
         document.querySelector('.top .high').textContent = `Max: ${Math.floor(high)}`
         document.querySelector('.top .low').textContent = `Min: ${Math.floor(low)}`
+
+      } else {
+        const dayClone = container.querySelector('.day').cloneNode(true)
+
+        dayClone.title = text
+        dayClone.querySelector('.date').textContent = date
+        dayClone.querySelector('.weather .wi').classList = `wi ${icon}`
+        dayClone.querySelector('.high').textContent = Math.floor(high)
+        dayClone.querySelector('.low').textContent = Math.floor(low)
+        dayClone.classList.add('day-clone');
+        container.querySelector('.bottom').appendChild(dayClone)
       }
 
-      const dayClone = container.querySelector('.day').cloneNode(true)
-
-      dayClone.querySelector('.date').textContent = new Date(formatedDate).toLocaleString(chrome.i18n.getUILanguage(), {weekday:'short'})
-      dayClone.querySelector('.weather img').src = `https://openweathermap.org/img/wn/${icon}@2x.png`
-      dayClone.querySelector('.high').textContent = Math.floor(high)
-      dayClone.querySelector('.low').textContent = Math.floor(low)
-      dayClone.classList.add('day-clone');
-      container.querySelector('.bottom').appendChild(dayClone)
     }
   }
 
--- src/modules/weather.js
+++ src/modules/weather.js
@@ -1,43 +1,127 @@
 class Weather {
-
   constructor() {
     this.BASE = {
-      api: 'https://api.openweathermap.org/data/2.5/',
-      units: 'imperial',
-      key: '71e8c71c98e8595d6727bb840067ad93'
+      api: "https://weather-ydn-yql.media.yahoo.com/forecastrss",
+      key: "dj0yJmk9Z1phRTFOaFNyQlV1JmQ9WVdrOU5WcGlabU5vTldrbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWY0",
+      secret: "078bdb02aae66633dd6697d93ce183172a5b5e29",
+      appId: '5Zbfch5i'
+    };
+
+    this.weatherIcon = {
+      0: 'wi-tornado',
+      1: 'wi-storm-showers',
+      2: 'wi-tornado',
+      3: 'wi-thunderstorm',
+      4: 'wi-thunderstorm',
+      5: 'wi-snow',
+      6: 'wi-rain-mix',
+      7: 'wi-rain-mix',
+      8: 'wi-sprinkle',
+      9: 'wi-sprinkle',
+      10: 'wi-hail',
+      11: 'wi-showers',
+      12: 'wi-showers',
+      13: 'wi-snow',
+      14: 'wi-storm-showers',
+      15: 'wi-snow',
+      16: 'wi-snow',
+      17: 'wi-hail',
+      18: 'wi-hail',
+      19: 'wi-cloudy-gusts',
+      20: 'wi-fog',
+      21: 'wi-fog',
+      22: 'wi-fog',
+      23: 'wi-cloudy-gusts',
+      24: 'wi-cloudy-windy',
+      25: 'wi-thermometer',
+      26: 'wi-cloudy',
+      27: 'wi-night-cloudy',
+      28: 'wi-day-cloudy',
+      29: 'wi-night-cloudy',
+      30: 'wi-day-cloudy',
+      31: 'wi-night-clear',
+      32: 'wi-day-sunny',
+      33: 'wi-night-clear',
+      34: 'wi-day-sunny-overcast',
+      35: 'wi-hail',
+      36: 'wi-day-sunny',
+      37: 'wi-thunderstorm',
+      38: 'wi-thunderstorm',
+      39: 'wi-thunderstorm',
+      40: 'wi-storm-showers',
+      41: 'wi-snow',
+      42: 'wi-snow',
+      43: 'wi-snow',
+      44: 'wi-cloudy',
+      45: 'wi-lightning',
+      46: 'wi-snow',
+      47: 'wi-thunderstorm',
+      3200: 'wi-cloud',
     }
+
+  }
+
+  async getYahooWeather(pos) {
+    const url = this.BASE.api
+    const method = 'GET'
+    const consumer_key = this.BASE.key
+    const consumer_secret = this.BASE.secret
+    const concat = '&'
+    const query = {'format': 'json', 'lat': pos.coords.latitude, 'lon': pos.coords.longitude};
+    const oauth = {
+        'oauth_consumer_key': consumer_key,
+        'oauth_nonce': Math.random().toString(36).substring(2),
+        'oauth_signature_method': 'HMAC-SHA1',
+        'oauth_timestamp': parseInt(new Date().getTime() / 1000).toString(),
+        'oauth_version': '1.0'
+    };
+
+    const merged = Object.assign({}, query, oauth)
+    // Note the sorting here is required
+    const merged_arr = Object.keys(merged).sort().map((k) => {
+      return [k + '=' + encodeURIComponent(merged[k])]
+    })
+    const signature_base_str = `${method}&${encodeURIComponent(url)}&${encodeURIComponent(merged_arr.join(concat))}`
+
+    const composite_key = `${encodeURIComponent(consumer_secret)}&`
+    const hash = CryptoJS.HmacSHA1(signature_base_str, composite_key)
+    const signature = hash.toString(CryptoJS.enc.Base64)
+
+    oauth['oauth_signature'] = signature
+    const auth_header = 'OAuth ' + Object.keys(oauth).map((k) => {
+      return [k + '="' + oauth[k] + '"']
+    }).join(',')
+
+    const resp = await fetch(`${url}?${new URLSearchParams(query).toString()}`, {
+      method: 'GET',
+      headers: {
+        'Authorization': auth_header,
+        'X-Yahoo-App-Id': this.BASE.appId
+      }
+    })
+    return await resp.json();
   }
 
   async getWeatherDataCache(key) {
-    return new Promise((done) => {
-      chrome.storage.local.get([key], (result) => {
-        done(result[key])
+    return new Promise(done => {
+      chrome.storage.local.get([key], result => {
+        if (!result[key]) {
+          console.log('no cache available!')
+        } else {
+          console.log('load cached weather.')
+        }
+        done(result[key]);
       });
-    })
-  }
-
-  get5DaysWeather() {
-    return new Promise((done) => {
-      navigator.geolocation.getCurrentPosition(async (pos) => {
-        const api = `${this.BASE.api}forecast?units=${this.BASE.units}&lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=${this.BASE.key}`
-        const resp = await fetch(api)
-        const weather = await resp.json()
-        chrome.storage.local.set({'5DaysWeather': JSON.stringify(weather)})
-        done(weather)
-      })
-    })
+    });
   }
 
   getCurrentWeather() {
     return new Promise((done) => {
       navigator.geolocation.getCurrentPosition(async (pos) => {
-        const api = `${this.BASE.api}weather?&units=${this.BASE.units}&lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&APPID=${this.BASE.key}`
-        const resp = await fetch(api)
-        const weather = await resp.json()
+        const weather = await this.getYahooWeather(pos)
         chrome.storage.local.set({'CurrentWeather': JSON.stringify(weather)})
         done(weather)
-      })
-    })
+      });
+    });
   }
-
 }
